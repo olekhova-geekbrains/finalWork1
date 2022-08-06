f8cf2f9e35f870c11a18f3e844d32aa21d940a68
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-08-06 21:21:15 +0300

    add method Create

--- Task/Program.cs
+++ Task/Program.cs
@@ -9,6 +9,19 @@ string Print(string[] arr)
     return result;
 }
 
+string[] Create()
+{
+    Console.WriteLine("Введите число - длина массива:");
+    int len = Convert.ToInt32(Console.ReadLine());
+    string[] arr = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива:");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
 int GetCountShortStrings(string[] arr, int reqLength)
 {
     int count = 0;
@@ -35,14 +48,26 @@ string[] GetShortStrings(string[] arr, int reqLength)
     return outputArray;
 }
 
+Console.WriteLine("Тестовые примеры");
 Console.WriteLine(Print(array1));
 string[] result1 = GetShortStrings(array1, requiredLength);
+Console.WriteLine($"Элементы массива, длина которых меньше либо равна {requiredLength}:");
 Console.WriteLine(Print(result1));
 Console.WriteLine();
 Console.WriteLine(Print(array2));
 string[] result2 = GetShortStrings(array2, requiredLength);
+Console.WriteLine($"Элементы массива, длина которых меньше либо равна {requiredLength}:");
 Console.WriteLine(Print(result2));
 Console.WriteLine();
 Console.WriteLine(Print(array3));
 string[] result3 = GetShortStrings(array3, requiredLength);
+Console.WriteLine($"Элементы массива, длина которых меньше либо равна {requiredLength}:");
 Console.WriteLine(Print(result3));
+Console.WriteLine();
+
+Console.WriteLine("Пользовательский ввод элементов массива");
+string[] myArray = Create();
+Console.WriteLine(Print(myArray));
+string[] result4 = GetShortStrings(myArray, requiredLength);
+Console.WriteLine($"Элементы массива, длина которых меньше либо равна {requiredLength}:");
+Console.WriteLine(Print(result4));
