67c5254ad5bbe92abd08d324308b46e48a11212a
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-08-06 18:57:09 +0300

    add method GetCountShortStrings

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿int requiredLength = 3;
 string[] array1 = { "hello", "2", "world", ":-)" };
 string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
@@ -7,6 +7,16 @@ string Print(string[] arr)
 {
     string result = $"[{string.Join(", ", arr)}]";
     return result;
+}
+
+int GetCountShortStrings(string[] arr, int reqLength)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= reqLength) count++;
+    }
+    return count;
 }
 
 Console.WriteLine(Print(array1));
