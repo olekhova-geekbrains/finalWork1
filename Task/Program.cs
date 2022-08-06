01ec4fbc7cccb27c984fb07e79946b022f9eabfb
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-08-06 19:14:16 +0300

    add method GetShortStrings

--- Task/Program.cs
+++ Task/Program.cs
@@ -19,4 +19,30 @@ int GetCountShortStrings(string[] arr, int reqLength)
     return count;
 }
 
+string[] GetShortStrings(string[] arr, int reqLength)
+{
+    int count = GetCountShortStrings(arr, reqLength);
+    string[] outputArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= reqLength)
+        {
+            outputArray[j] = arr[i];
+            j++;
+        }
+    }
+    return outputArray;
+}
+
 Console.WriteLine(Print(array1));
+string[] result1 = GetShortStrings(array1, requiredLength);
+Console.WriteLine(Print(result1));
+Console.WriteLine();
+Console.WriteLine(Print(array2));
+string[] result2 = GetShortStrings(array2, requiredLength);
+Console.WriteLine(Print(result2));
+Console.WriteLine();
+Console.WriteLine(Print(array3));
+string[] result3 = GetShortStrings(array3, requiredLength);
+Console.WriteLine(Print(result3));
